cf2b239235489a4d62e3ff2adabd4ed3e3e0343c
Derlis Damian Meza Carballo <derlisdamianmeza@gmail.com>
2024-03-31 19:53:17 -0400

    creada ruta para obtener la autorizacion de airtable
    esta ruta sirve para obtener el codigo de autorizacion para poder hacer que el usuario de permiso a la aplicacion para usar uno de sus workspaces

---
+++ src/app/api/airtable/authorize/route.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+import {
+  generateCodeVerifier,
+  generateState,
+} from "@/helpers/generateRandomCode";
+import { encodeToBase64UrlSha256 } from "@/helpers/codifyCodeChallenge";
+
+export async function GET() {
+  const state = generateState();
+  const codeVerifier = generateCodeVerifier();
+  const codeChallenge = encodeToBase64UrlSha256(codeVerifier);
+
+  const airtableResponse = await axios.get(
+    "https://airtable.com/oauth2/v1/authorize",
+    {
+      params: {
+        client_id: process.env.AIRTABLE_CLIENT_ID,
+        response_type: "code",
+        redirect_uri: process.env.AIRTABLE_REDIRECT_URI,
+        state: state,
+        code_challenge: codeChallenge,
+        code_challenge_method: "S256",
+        scope:
+          "data.records:read data.records:write schema.bases:read schema.bases:write",
+      },
+    },
+  );
+
+  if (airtableResponse.data.state !== state || airtableResponse.data.code_challenge !== codeChallenge) {
+    return new Response("Error", { status: 500 });
+  }
+
+  return new Response(airtableResponse.data.code);
+}
