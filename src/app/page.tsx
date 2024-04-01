1845dbcd77b4afa2483183702ba7320ecb63fb83
Derlis Damian Meza Carballo <derlisdamianmeza@gmail.com>
2024-03-31 21:30:03 -0400

    intentando autorizar airtable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,58 @@
 "use client";
 import { Session } from "inspector";
-// pages/index.js
-import { useSession, signIn, signOut } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 import { useEffect } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import airtableAuth from "@/helpers/airtableAuth";
+
+type InputData = {
+  rubro: string;
+  nombreTienda: string;
+  horario: string;
+  comportamientoAsistente: string;
+};
 
 type userInfo = {
-  "id": string;
-  "createdAt": string;
-  "updatedAt": string;
-  "AssistantID": string;
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  AssistantID: string;
+  "Personalizacion del Asistente": InputData;
   "Personal Access Token": string;
-  "BaseClientesID": string;
-  "ConversacionesIniciadas": number;
+  RefreshToken: string;
+  BaseClientesID: string;
+  ConversacionesIniciadas: number;
   Email: string;
-}
+};
 
 type userInfoServicio = userInfo & {
-  "WorkspaceID": string;
-  "BaseAgendaID": string;
-  "BaseServiciosID": string;
-  "BaseEmpleadosID": string;
-}
+  WorkspaceID: string;
+  BaseAgendaID: string;
+  BaseServiciosID: string;
+  BaseEmpleadosID: string;
+};
 
 type userInfoConsumo = userInfoServicio & {
-  "RefreshToken": string;
-  "BasePedidosID": string;
-  "BaseProductosID": string;
-}
+  BasePedidosID: string;
+  BaseProductosID: string;
+};
 
 type UserSession = Session & {
   accessToken: string;
   refreshToken: string;
+  user: {
+    name: string;
+    email: string;
+    image: string;
+  };
   userInfo: userInfoServicio | userInfoConsumo;
-  assistantType: string;
+  assistantType: "servicio" | "consumo";
 };
-
 
 export default function Home() {
   const datos = useSession();
-  const { data: session } = datos;
+  const session = datos.data as UserSession | null;
+
   useEffect(() => {
     console.log(datos);
   }, [datos]);
@@ -58,6 +72,8 @@ export default function Home() {
                 {session.user.name}
               </p>
             </div>
+            {session.assistantType === "consumo" && <Consumo />}
+            {session.assistantType === "servicio" && <Servicio />}
             <button
               className={"bg-red-500 rounded-md"}
               onClick={() => signOut()}
@@ -77,4 +93,54 @@ export default function Home() {
     </div>
   );
 }
+
+function FormData({}) {
+  const { handleSubmit, register } = useForm<InputData>();
+
+  const onSubmit: SubmitHandler<InputData> = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <>
+      <button onClick={airtableAuth}>Habilitar Airtable</button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>
+          Rubro:
+          <input type="text" {...register("rubro")} />
+        </label>
+        <label>
+          Nombre de la tienda:
+          <input type="text" {...register("nombreTienda")} />
+        </label>
+        <label>
+          Horario:
+          <input type="text" {...register("horario")} />
+        </label>
+        <label>
+          Comportamiento del asistente:
+          <input type="text" {...register("comportamientoAsistente")} />
+        </label>
+        <input type="submit" />
+      </form>
+    </>
+  );
+}
+
+function Consumo({}) {
+  return (
+    <div className="text-black">
+      <p>Consumo</p>
+      <FormData />
+    </div>
+  );
+}
+
+function Servicio({}) {
+  return (
+    <div className="text-black">
+      <p>Servicio</p>
+      <button>Habilitar Airtable</button>
+    </div>
+  );
 }
